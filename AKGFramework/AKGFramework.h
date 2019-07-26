5aa610c95e790fc6e48c7ce8c0d6c67bf2b27af6
Lutfi Azhar <m.lutfiazhar@gmail.com>
2019-07-26 14:05:13 +0700

    Initial Commit

---
+++ AKGFramework/AKGFramework.h
@@ -0,0 +1,19 @@
+//
+//  AKGFramework.h
+//  AKGFramework
+//
+//  Created by Lutfi Azhar on 26/07/19.
+//  Copyright © 2019 Lutfi Azhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKGFramework.
+FOUNDATION_EXPORT double AKGFrameworkVersionNumber;
+
+//! Project version string for AKGFramework.
+FOUNDATION_EXPORT const unsigned char AKGFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKGFramework/PublicHeader.h>
+
+
